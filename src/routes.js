a04f469208aa32d102bb0330f69be70304702c6f
iamrosada <luisrosada2018@gmail.com>
2022-04-12 12:57:25 +0300

    feature: created new router edit page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { AllStudents} from './pages/allStudents/index'
+import EditStudents from "./pages/editStudents";
 export const RoutesFunction = () => {
   return (
     <Routes>
       <Route exact path='/about' element={<AllStudents />} />
+      <Route exact path='/edit' element={<EditStudents />} />
       {/* <Route path="/users"></Route> */}
       {/* <Route path="/"></Route> */}
     </Routes>
