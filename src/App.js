0e435f7b5c9a62cae6ba6dff59fe78e66e430791
iamrosada <luisrosada2018@gmail.com>
2022-04-12 12:04:25 +0300

    feature: added router and provider for use Context api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import NavbarGlobal from "./components/content";
 import SidebarWithHeader from "./components/sidebar";
 import "./styles/global/style.css";
-
+import { BrowserRouter as Router } from "react-router-dom";
+import { RoutesFunction } from "./routes";
+import { useState } from "react";
+import AppContext from './AppContext'
+const {Provider} = AppContext;
 function App() {
+  const [openPage, setOpenPage] = useState(false)
   return (
-    <>
-      {/* <NavbarGlobal /> */}
-      <Content />
-      <SidebarWithHeader />
-    </>
+<Provider value={[openPage,setOpenPage]}>
+       <Router>
+          {/* <NavbarGlobal /> */}
+          {/* <Content /> */}
+          
+          <SidebarWithHeader>
+          <RoutesFunction />
+          </SidebarWithHeader>
+       
+      </Router>
+</Provider>
+     
+ 
   );
 }
 
-const Content = () => {
-  return <div id="content"></div>;
-};
+// const Content = () => {
+//   return <div id="content"></div>;
+// };
 
 export default App;
