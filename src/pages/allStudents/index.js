fda19350c3eba87d2dcd14baa35bd76aff6f2502
iamrosada <luisrosada2018@gmail.com>
2022-04-17 11:36:24 +0300

    feature: updated allStudents

--- src/pages/allStudents/index.js
+++ src/pages/allStudents/index.js
@@ -1,8 +1,76 @@
+import "./styles.css";
+const studentArray = [
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
 
-
-export const AllStudents = () =>{
-
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+];
+export const AllStudents = () => {
   return (
-    <h1>ALL STUDENT</h1>
-  )
-}
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>ID</th>
+        <th>Class</th>
+        <th>Age</th>
+        <th>Gender</th>
+        <th>Email</th>
+      </tr>
+
+      {studentArray.map((stu) => {
+        return (
+          <tr key={stu.ID}>
+            <td>
+              <div className="avatar-student">
+                <img src={stu.avatar} alt="" />
+                <span>{stu.Name}</span>
+              </div>
+            </td>
+            <td>{stu.ID}</td>
+            <td>{stu.Class}</td>
+            <td>{stu.Age}</td>
+            <td>{stu.Gender}</td>
+            <td>{stu.Email}</td>
+          </tr>
+        );
+      })}
+    </table>
+  );
+};
