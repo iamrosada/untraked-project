43283c2afae99f860342cc104ba6fe12fe2145e6
iamrosada <luisrosada2018@gmail.com>
2022-04-17 11:34:49 +0300

    feature: created page to delete the students

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { AllStudents} from './pages/allStudents/index'
+import DeleteStudent from "./pages/deleteStudents";
 import EditStudents from "./pages/editStudents";
 export const RoutesFunction = () => {
   return (
     <Routes>
       <Route exact path='/about' element={<AllStudents />} />
       <Route exact path='/edit' element={<EditStudents />} />
-      {/* <Route path="/users"></Route> */}
+      <Route exact path='/delete' elemen={<DeleteStudent/>}   />   {/* <Route path="/users"></Route> */}
       {/* <Route path="/"></Route> */}
     </Routes>
   );
