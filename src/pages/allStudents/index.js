f82c132935f60036a6a8b7ece2b94dbbc3dfae73
iamrosada <luisrosada2018@gmail.com>
2022-04-18 11:28:30 +0300

    feature: created page for all students

--- src/pages/allStudents/index.js
+++ src/pages/allStudents/index.js
@@ -1,3 +1,4 @@
+import BioStudents from "../../components/bioStudents/bioStudents";
 import "./styles.css";
 const studentArray = [
   {
@@ -41,36 +42,170 @@ const studentArray = [
     avatar:
       "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
   },
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    Name: "luis rosada",
+    ID: "197190",
+    Class: "y-196",
+    Age: "21",
+    Gender: "Male",
+    Email: "luis@gmail.ru",
+    avatar:
+      "https://yt3.ggpht.com/H1JFb8NCxufgD7KeXPzgfuKJdJlfTmuliyHutasUm1wHOZ5QXj6KEIin2QXdG8MAYFTpRPT0Wg=s88-c-k-c0x00ffffff-no-rj-mo",
+  },
 ];
 export const AllStudents = () => {
   return (
-    <table>
-      <tr>
-        <th>Name</th>
-        <th>ID</th>
-        <th>Class</th>
-        <th>Age</th>
-        <th>Gender</th>
-        <th>Email</th>
-      </tr>
+    <section className="container-all-students-and-bio">
+      <table>
+        <tr className="info-section">
+          <th>Name</th>
+          <th>ID</th>
+          <th>Class</th>
+          <th>Age</th>
+          <th>Gender</th>
+          <th>Email</th>
+        </tr>
 
-      {studentArray.map((stu) => {
-        return (
-          <tr key={stu.ID}>
-            <td>
-              <div className="avatar-student">
-                <img src={stu.avatar} alt="" />
-                <span>{stu.Name}</span>
-              </div>
-            </td>
-            <td>{stu.ID}</td>
-            <td>{stu.Class}</td>
-            <td>{stu.Age}</td>
-            <td>{stu.Gender}</td>
-            <td>{stu.Email}</td>
-          </tr>
-        );
-      })}
-    </table>
+        {studentArray.map((stu) => {
+          return (
+            <tr key={stu.ID} id="tr-students">
+              <td className="container-td">
+                <div className="avatar-student">
+                  <img src={stu.avatar} alt="" />
+                  <span>{stu.Name}</span>
+                </div>
+              </td>
+              <td>{stu.ID}</td>
+              <td>{stu.Class}</td>
+              <td>{stu.Age}</td>
+              <td>{stu.Gender}</td>
+              <td>{stu.Email}</td>
+            </tr>
+          );
+        })}
+      </table>
+      <BioStudents />
+    </section>
   );
 };
